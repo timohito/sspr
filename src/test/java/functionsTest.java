9ebf3a7f24e8a8cc07e91a239445a817825a9d07
timohito <71021976+timohito@users.noreply.github.com>
2022-10-10 11:22:35 +0400

    Update functionsTest.java

--- src/test/java/functionsTest.java
+++ src/test/java/functionsTest.java
@@ -1,14 +1,16 @@
+import org.example.Functions;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import java.io.IOException;
+import static org.example.Functions.*;
 
 public class functionsTest {
+    Functions functions = new Functions();
     @Test
     public void sumTest() {
         int a = 5;
         int b = 10;
-        int sum = sum(a,b);
+        int sum = functions.sum(a,b);
         Assertions.assertEquals(15, sum);
     }
 
@@ -16,7 +18,7 @@ public class functionsTest {
     public void subTest() {
         int a = 25;
         int b = 10;
-        int sub = sub(a,b);
+        int sub = functions.sub(a,b);
         Assertions.assertEquals(15, sub);
     }
 
@@ -24,7 +26,7 @@ public class functionsTest {
     public void mulTest() {
         int a = 25;
         int b = 10;
-        int mul = mul(a,b);
+        int mul = functions.mul(a,b);
         Assertions.assertEquals(250, mul);
     }
 
@@ -32,7 +34,7 @@ public class functionsTest {
     public void divTest() {
         int a = 55;
         int b = 5;
-        int div = (int) div(a,b);
+        int div = (int) functions.div(a,b);
         Assertions.assertEquals(11, div);
     }
 }
